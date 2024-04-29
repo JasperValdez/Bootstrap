50edc5f7ffd43dc25304ca0587fd9954885319bd
Jasper F. Valdez <jfvaldez.wup@gmail.com>
2024-04-29 09:36:33 +0800

    bootstrap1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+$("document").ready(() =>{
 console.log("Hello World");
+});
 let btnCreate = document.querySelector('#btnCreate');
 btnCreate.onclick = function() {
     let floatingProductName = document.querySelector("#floatingProductName");
@@ -14,6 +16,64 @@ function createProduct(productName, productPrice) {
         price:productPrice,
     };
 
-localStorage.setItem("product",JSON.stringify(product));
-console.log("Creating product..." , productName, productPrice);
+    //get the initial value of products array
+    let products = localStorage.getItem("product");
+    
+    //check if array is empty
+    if(!products) products = [];
+    else products = JSON.parse(products) //Parse to JSON object if not null.
+
+    // validate if product name is already exsting
+    let productIsExisting =products.findIndex((p) => {
+    return p.name == product.name;
+
+    });
+
+    if(productIsExisting >= 0)
+    return alert (`${product.name} is already in the database.`);
+
+    products.push(product); // add sa array
+
+
+//save sa database
+localStorage.setItem("product",JSON.stringify(products));
+alert(`${product.name} has been successfully added`);
+console.log("Creating product....", productName, productPrice);
+
+}
+
+//read
+function getProducts() {
+    $("#tbodyProducts").html("");
+
+//get the initial value of products array
+let products = localStorage.getItem("product");
+
+//check if array is empty
+if(!products) products = [];
+else products = JSON.parse(products)
+
+products.forEach((p) => {
+    $("#tbodyProducts").append(
+    `<tr>
+    <td>${p.name}</td>
+    <td>${p.price}</td>
+    <td><button> class "btn btn-primary ">Update</button>
+    <button> class "btn btn-danger" onclick="deleteProduct(${i})">Delete</button></td>
+</tr>`
+    );
+});
+
+}
+
+function deleteProduct(productIndex) {
+let products = localStorage.getItem("product");
+if(!products) products =[];
+else products =JSON.parse(products);
+
+products[productIndex].isDeleted = true;
+
+
+localStorage.setItem("product", JSON.stringify(products));
+getProducts();
 }
